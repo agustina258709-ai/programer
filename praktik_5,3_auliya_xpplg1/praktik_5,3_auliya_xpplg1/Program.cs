e12a193f4a5665935141e7075c0e289a8df66507
agustina258709-ai <agustina258709@student.smkn1kandeman.sch.id>
2025-11-03 14:32:04 +0700

    Add files via upload

---
+++ praktik_5,3_auliya_xpplg1/praktik_5,3_auliya_xpplg1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5_3_auliya_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // inisialisasi variable untuk menyimpan input
+            string inputUser = "";
+
+                Console.WriteLine("program akan terus berjalan hingga anda mengetik 'keluar' . ");
+
+            // perulangan selama isi variable inputUser TIDAK SAMA DENGAN "keluar"
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.Write("\nketik sesuatu (atau 'keluar' untuk berhenti): ");
+
+                inputUser = Console.ReadLine(); // baca input dari user
+
+                Console.WriteLine("anda mengetik: " + inputUser);
+            }
+            Console.WriteLine("\nprogres selesai.terimakasih");
+        }
+    }
+}
