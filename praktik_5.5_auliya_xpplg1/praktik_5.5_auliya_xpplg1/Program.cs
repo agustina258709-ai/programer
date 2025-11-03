d864cbf2cdd0fc70025a04c622b8901756854dc7
agustina258709-ai <agustina258709@student.smkn1kandeman.sch.id>
2025-11-03 14:28:21 +0700

    Add files via upload

---
+++ praktik_5.5_auliya_xpplg1/praktik_5.5_auliya_xpplg1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5._5_auliya_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
