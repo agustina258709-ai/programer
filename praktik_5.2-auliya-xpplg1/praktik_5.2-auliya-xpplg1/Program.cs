09df753a1732218610e0d1c1047ed262ed3ce752
agustina258709-ai <agustina258709@student.smkn1kandeman.sch.id>
2025-11-03 14:32:36 +0700

    Add files via upload

---
+++ praktik_5.2-auliya-xpplg1/praktik_5.2-auliya-xpplg1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5._2_auliya_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabel untuk menyimpan hasil penjumlahan
+            int total = 0;
+
+            Console.WriteLine("Menghitung jumlah angka dari 1 запраї 5:");
+
+            // Perulangan for dari 1 hingga 5
+            for (int i = 1; i <= 5; i++)
+            {
+                // Tambahkan nilai i saat ini ke variabel total
+                total = total + i; // Bisa juga ditulis sebagai total += i;
+                // Tampilkan proses penjumlahan di setiap langkah.
+                Console.WriteLine("Menambahkan " + i + ", total sementara= " + total);
+            }
+
+            // Tampilkan hasil akhir setelah perulangan selesai
+            Console.WriteLine("\nHasil akhir penjumlahan adalah: " + total);
+        }
+    }
+}
+
